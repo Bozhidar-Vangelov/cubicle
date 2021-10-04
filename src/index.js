96736f058312fd08fd132a2ea0e3a86563eb16cc
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-10-04 18:14:22 +0300

    Added start express app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-const env = process.env.NODE_ENV || 'development';
+const express = require('express');
 
-const config = require('./config/config')[env];
-const app = require('express')();
+const app = express();
 
-require('./config/express')(app);
-require('./config/routes')(app);
+app.all('/', (req, res) => {
+  res.write("It's working");
+  res.end();
+});
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.listen(
+  5000,
+  console.log.bind(console, 'Application is running on http://localhost:5000')
+);
