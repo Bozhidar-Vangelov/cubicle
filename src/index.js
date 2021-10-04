680c6cd0282258858b9a3931ff8e10fb8f9742c8
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-10-04 18:40:36 +0300

    Added handlebars config

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
 
 const app = express();
 
-app.set('views', path.resolve('./src/views'));
-app.engine(
-  'hbs',
-  handlebars({
-    extname: 'hbs',
-  })
-);
-app.set('view engine', 'hbs');
+require('./config/handlebars')(app);
 
 app.all('/', (req, res) => {
-  res.render('index', { layout: false });
-//   res.end();
+  res.render('index');
 });
 
 app.listen(
