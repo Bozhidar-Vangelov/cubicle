989cdab006539b3c5685e859dac8d2fc28bd5dd3
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-10-07 18:10:10 +0300

    Added search by text

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -9,10 +9,14 @@ const create = (name, description, imageUrl, difficulty) => {
   Cube.add(cube);
 };
 
+const search = (text, from, to) =>
+  Cube.cubes.filter((x) => x.name.toLowerCase().includes(text.toLowerCase()));
+
 const cubeService = {
   create,
   getAll,
   getOne,
+  search,
 };
 
 module.exports = cubeService;
