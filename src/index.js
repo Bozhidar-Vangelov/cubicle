70da404ebc7dda47fdef24d8bc7e25637fd743eb
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-10-22 16:04:11 +0300

    Added DB config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const routes = require('./routes.js');
 const config = require('./config/config.json')[process.env.NODE_ENV];
+const initDatabase = require('./config/database.js');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,10 +14,16 @@ require('./config/handlebars')(app);
 app.use(express.static(path.resolve(__dirname, './public')));
 app.use(routes);
 
-app.listen(
-  config.PORT,
-  console.log.bind(
-    console,
-    `Application is running on http://localhost:${config.PORT}`
-  )
-);
+initDatabase(config.DB_CONNECTION_STRING)
+  .then(() => {
+    app.listen(
+      config.PORT,
+      console.log.bind(
+        console,
+        `Application is running on http://localhost:${config.PORT}`
+      )
+    );
+  })
+  .catch((err) => {
+    console.log('Application init failed: ', err);
+  });
