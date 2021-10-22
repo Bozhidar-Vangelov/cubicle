ae2ea928fcef45865c9e9759fe67eb2619d840ba
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-10-22 14:40:07 +0300

    Added mongoose

---
+++ hcCubes.js
@@ -0,0 +1,29 @@
+[
+  {
+    id: 'skmadw88d1kdfmd2',
+    name: "Rubik's Cube",
+    description:
+      "The Rubik's Cube is a 3-D combination puzzle invented in 1974[2][3] by Hungarian sculptor and professor of architecture Ernő Rubik. Originally called the Magic Cube,[4] the puzzle was licensed by Rubik to be sold by Ideal Toy Corp. in 1980[5] via businessman Tibor Laczi and Seven Towns founder Tom Kremer.",
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Rubiks_cube_solved.jpg/220px-Rubiks_cube_solved.jpg',
+    difficulty: '3',
+  },
+  {
+    id: 'iesfo5c9kugzbaai',
+    name: 'Regular hexahedron ',
+    description:
+      'In geometry, a cube[1] is a three-dimensional solid object bounded by six square faces, facets or sides, with three meeting at each vertex. ',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Hexahedron.jpg/280px-Hexahedron.jpg',
+    difficulty: '6',
+  },
+  {
+    id: 'iesfo5mhkugzeseb',
+    name: 'Strange Cube',
+    description:
+      'Cheap CubeTwist Strange Shape Cube Sticker Square II SQ2 3x3x3 Speed Cube Sector Magic Cube Educational Puzzle Toys For Kids With Brac Wholesale. Buy Quality Magic Cubes Directly From SHIJUE TOY Store Suppliers. Enjoy ✓Free Shipping Worldwide! ✓Limited Time Sale ✓Easy Return.',
+    imageUrl:
+      'https://yearlyspring.news/img/products/43229-lanlan-ll-strange-shaped-magic-cube-speed-pzel-flower-petal-cubes-podark-za-rozhden-den-zabavni-igrachki-za-deca-decata-na-vzrastni.jpg',
+    difficulty: '1',
+  },
+];
