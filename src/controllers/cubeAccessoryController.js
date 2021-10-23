47ebd902cb381bc7c58fc01bc3c944fe9fcf1441
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-10-23 14:18:28 +0300

    Added attach accessory page

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
-router.get('/add-accessory', (req, res) => {
-  console.log(req.params.cubeId);
+const cubeService = require('../services/cubeService.js');
 
-  res.end();
+router.get('/add-accessory', async (req, res) => {
+  let cube = await cubeService.getOne(req.params.cubeId);
+
+  res.render('cube/attachAccessory', { ...cube });
 });
 
 module.exports = router;
