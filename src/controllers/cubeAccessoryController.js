40a4e8cbeccfa4e3d938600a19714f0ffd8e7610
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-10-23 19:01:13 +0300

    Added filter to show only unattached accessories

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -6,9 +6,9 @@ const accessoryService = require('../services/accessoryService.js');
 
 router.get('/add-accessory', async (req, res) => {
   let cube = await cubeService.getOne(req.params.cubeId);
-  let accessories = await accessoryService.getAll();
-
-  console.log(accessories);
+  let accessories = await accessoryService.getAllWithout(
+    cube.accessories.map((x) => x._id)
+  );
 
   res.render('cube/attachAccessory', { cube, accessories });
 });
