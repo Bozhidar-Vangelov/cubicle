072d9bdf8ae033762ff610585c95446c34966c41
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-10-23 14:31:23 +0300

    Show available accessories on accessory attach page

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const cubeService = require('../services/cubeService.js');
+const accessoryService = require('../services/accessoryService.js');
 
 router.get('/add-accessory', async (req, res) => {
   let cube = await cubeService.getOne(req.params.cubeId);
+  let accessories = await accessoryService.getAll();
 
-  res.render('cube/attachAccessory', { ...cube });
+  res.render('cube/attachAccessory', { cube, accessories });
 });
 
 module.exports = router;
