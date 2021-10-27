4e63c176cc88cd6228c5c0b6acb6ae8038118173
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-10-27 23:02:21 +0300

    Created authcontroller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,3 +3,5 @@ const router = require('express').Router();
 router.get('login', (req, res) => {
   res.render('auth/login');
 });
+
+module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const router = require('express').Router();
 
 const homeController = require('./controllers/homeController.js');
 const cubeController = require('./controllers/cubeController.js');
 const accessoryController = require('./controllers/accessoryController.js');
-
-const router = express.Router();
+const authController = require('./controllers/authController.js');
 
 router.use(homeController);
 router.use(cubeController);
 router.use(accessoryController);
+router.use(authController);
 router.use('*', (req, res) => {
   res.render('404');
 });
