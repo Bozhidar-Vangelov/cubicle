f06ba8327e98a95798486f33de3693ef0f5419f5
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-10-28 17:58:07 +0300

    Updated jwtUtils

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const { SECRET } = require('../constants.js');
 
 function promiseSign(payload, secret) {
   let promise = new Promise((resolve, reject) => {
@@ -14,18 +13,8 @@ function promiseSign(payload, secret) {
   return promise;
 }
 
-function createToken(user) {
-  let payload = {
-    _id: user.get('_id'),
-    username: user.get('username'),
-  };
-
-  return sign(payload, SECRET);
-}
-
 const jwtUtils = {
   promiseSign,
-  createToken,
 };
 
 module.exports = jwtUtils;
