42e3447e9830386ddc730fd4e7f633ef814c8655
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2021-10-28 19:25:10 +0300

    Added edit and delete cube pages

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -21,10 +21,19 @@ const getCubeDetails = async (req, res) => {
   res.render('cube/details', { ...cube });
 };
 
+const getEditCubePage = (req, res) => {
+  res.render('cube/edit');
+};
+
+const getDeleteCubePage = (req, res) => {
+  res.render('cube/delete');
+};
+
 router.get('/create/cube', getCreateCube);
 router.post('/create/cube', createCube);
 router.get('/details/:cubeId', getCubeDetails);
 router.use('/details/:cubeId/accessory', cubeAccessoryController);
+router.get('/edit/cube/:cubeId', getEditCubePage);
+router.get('/delete/cube/:cubeId', getDeleteCubePage);
 
 module.exports = router;
-
